bf4c27bb1c4f7e31556b3574b90d2e6e1d7588b2
陈志远 <chenzy01@eastcom-sw.com>
2020-06-16 16:16:50 +0800

    :sparkles: 服务注册中心（单节点模式）

---
+++ learn-eureka/eureka-server/src/test/java/com/nasus/eurekaserver/EurkaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasus.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurkaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- learn-hello/src/main/java/com/nasus/hello/web/HelloController.java
+++ learn-hello/src/main/java/com/nasus/hello/web/HelloController.java
@@ -1,7 +1,13 @@
 package com.nasus.hello.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Project Name:sclearn <br/>
@@ -13,11 +19,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // 服务注册
+    @Autowired
+    private Registration registration;
+
+    @Autowired
+    private DiscoveryClient client;
+
     @GetMapping("/hello")
     public String index(){
-
+        ServiceInstance instance = serviceInstance();
+        String x = String.format("Host: %s, ServiceId: %s\n", instance.getHost(), instance.getServiceId());
+        System.out.println(x);
         return "Hello World!";
 
     }
 
+
+    /**
+     * 获取当前服务实例
+     * @return
+     */
+    public ServiceInstance serviceInstance() {
+        List<ServiceInstance> list = client.getInstances(registration.getServiceId());
+        if (list != null && list.size() > 0) {
+            for(ServiceInstance itm : list){
+                if(itm.getPort() == 8888)
+                    return itm;
+            }
+        }
+        return null;
+    }
 }
